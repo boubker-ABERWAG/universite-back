37a3242ea60538b007dd71d437d85cc4e7d62b36
Boubker ABERWAG <b_aberwag@yahoo.fr>
2019-03-15 09:04:15 +0100

    init universite back

---
+++ src/main/java/fr/aberwag/universite/enseignant/controller/IEnseignantController.java
@@ -0,0 +1,13 @@
+package fr.aberwag.universite.enseignant.controller;
+
+import java.util.List;
+
+import fr.aberwag.universite.enseignant.domain.Enseignant;
+
+public interface IEnseignantController {
+	public List<Enseignant> findAll();
+
+	public Enseignant findOne(Integer id);
+
+	public Enseignant save(Enseignant enseignant);
+}
---
+++ src/main/java/fr/aberwag/universite/note/domain/NotePK.java
@@ -0,0 +1,36 @@
+package fr.aberwag.universite.note.domain;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+
+@Embeddable
+public class NotePK  implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+
+	@Column(name="ID_ETUDIANT")
+	private Integer etudiant;
+	
+	@Column(name="ID_MATIERE")
+	private Integer matiere;
+
+	public Integer getEtudiant() {
+		return etudiant;
+	}
+
+	public void setEtudiant(Integer etudiant) {
+		this.etudiant = etudiant;
+	}
+
+	public Integer getMatiere() {
+		return matiere;
+	}
+
+	public void setMatiere(Integer matiere) {
+		this.matiere = matiere;
+	}
+	
+	
+}
